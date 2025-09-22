3de57c22812c8c24dcdb6a7388c7a577398979bc
jhjang <jjlee@softhills.net>
2025-09-22 10:53:29 +0900

    Source 수정
    HMF 중립포멧 로딩

--- src/VIZWeb3D/VIZWeb3D/backup_simulation/ReviewTree.js
+++
@@ -1,8 +0,0 @@
-﻿VIZWeb3D.ReviewTree = function (View) {
-    var scope = this;
-    var view = View;
-    this.View = view;
-    var datamng = view.Data;
-    var visible = false;
-    var init = false;
-};
